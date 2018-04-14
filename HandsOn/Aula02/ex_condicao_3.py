fc1282ea0ef0e157a36cb7ed9d3e1d81893d1189
Luiz Ferrarezi <lferrarezi@gmail.com>
2018-04-14 09:58:06 -0300

    Aula 1

---
+++ HandsOn/Aula02/ex_condicao_3.py
@@ -0,0 +1,16 @@
+# If ternário
+# não possui elif
+
+lista = [0,1,2,3,4,9,87,10]
+# len() retorna a quantidade de itens na lista
+tamanho = 0
+
+#r = 3 % 2
+#print(r)
+
+while tamanho < len(lista):
+    print("par" if lista[tamanho] % 2 == 0 else "impar")
+    tamanho += 1
+
+for numero in lista:
+    print("par" if numero % 2 == 0 else "impar")
